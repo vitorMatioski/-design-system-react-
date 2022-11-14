9fa1b7d39061bd4474c79a39503152b9ee778a3a
Vitor Matheus Matioski <vitor.m0520@gmail.com>
2022-11-14 09:28:46 -0300

    first commit

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,22 @@
+import * as CheckboxPrimite from '@radix-ui/react-checkbox'
+import {Check} from "phosphor-react"
+
+export interface CheckboxProps{
+
+}
+
+export function Checkbox({}:CheckboxProps){
+     return(
+         <CheckboxPrimite.Root
+         className='w-6 h-6 p-[2px] bg-gray-800 rounded'
+         >
+          <CheckboxPrimite.Indicator asChild>
+               <Check weight='bold' className='h-5 w-5 text-cyan-500 '/>
+          </CheckboxPrimite.Indicator>
+
+         </CheckboxPrimite.Root>
+
+
+
+     )
+}
